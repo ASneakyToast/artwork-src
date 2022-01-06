0eb55b0a275b15c586324afa2ea9cf1b3b4194fa
Joel Lithgow <joelnotlithgow@gmail.com>
2022-01-05 20:52:52 -0800

    Added github pages repo to router config.

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: '/artwork-prod/'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Artwork',
